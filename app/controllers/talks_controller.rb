67b20b7c7e7992eaa454b14dc3375674d24e8420
watanabefg <h.w.watanabe@gmail.com>
2018-05-02 12:47:39 +0000

    create branch

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -3,6 +3,18 @@ class TalksController < ApplicationController
 
   # POST /talks
   def create
-    Rails.logger.info params
+    request = AlexaRubykit::build_request(params)
+    
+    response = AlexaRubykit::Response.new
+    
+    if request.slots[:weight][:value]
+      response.add_speech("#{request.slots[:weight][:value]}キログラムですね。")
+    end
+    
+    if request.slots[:bodyfatpercentage][:value]
+      response.add_speech("#{request.slots[:bodyfatpercentage][:value]}パーセントですね。")
+    end
+    
+    render json: response.build_response
   end
 end
