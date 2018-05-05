423d32067766d8b81739ac71bc1673c55de176ba
watanabefg <h.w.watanabe@gmail.com>
2018-05-05 13:50:36 +0000

    add responses

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -6,13 +6,19 @@ class TalksController < ApplicationController
     request = AlexaRubykit::build_request(params)
     
     response = AlexaRubykit::Response.new
+    session_end = true
     
-    if request.slots[:weight][:value]
-      response.add_speech("#{request.slots[:weight][:value]}キログラムですね。")
-    end
-    
-    if request.slots[:bodyfatpercentage][:value]
-      response.add_speech("#{request.slots[:bodyfatpercentage][:value]}パーセントですね。")
+    case request.name
+      when 'WeightManagement'
+        weight = request.slots[:weight][:value]
+        response.add_speech("#{weight}キログラムですね。体脂肪率は何％ですか？")
+        response.add_session_attribute :weight, weight
+        session_end = false
+      when 'BodyfatpercentageManagement'
+        weight = request.session.attributes[:weight]
+        bodyfatpercentage = request.slots[:bodyfatpercentage][:value]
+        # TODO: DB登録処理を入れる
+        response.add_speech("#{bodyfatpercentage}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
     end
     
     render json: response.build_response
