be7055e2dc6a3f6b9b6b8f17f7c634f9e9f14b9d
watanabefg <h.w.watanabe@gmail.com>
2018-05-06 02:09:52 +0000

    他のインテントに対応

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -8,27 +8,30 @@ class TalksController < ApplicationController
     response = AlexaRubykit::Response.new
     session_end = true
     
-    response.add_speech("#{request.intent.name}")
-    
     case request.type
       when 'LAUNCH_REQUEST'
         response.add_speech("スマートバルクへようこそ。体重と体脂肪率の管理をします。管理をしますか？")
         session_end = false
       when 'INTENT_REQUEST'
-        case request.intent.name 
+        case request.intent[:name]
+          when 'AMAZON.YesIntent'
+            response.add_speech("体重は何キログラムですか？")
+            session_end = false
+          when 'AMAZON.NoIntent'
+            response.add_speech("ご利用ありがとうございました。またスマートバルクをご利用ください。")
           when 'WeightManagement'
-            weight = request.intent.slots[:weight][:value]
+            weight = request.slots[:weight][:value]
             response.add_speech("#{weight}キログラムですね。体脂肪率は何％ですか？")
             response.add_session_attribute :weight, weight
             session_end = false
           when 'BodyfatpercentageManagement'
             weight = request.session.attributes[:weight]
-            bodyfatpercentage = request.intent.slots[:bodyfatpercentage][:value]
+            bodyfatpercentage = request.slots[:bodyfatpercentage][:value]
             # TODO: DB登録処理を入れる
             response.add_speech("#{bodyfatpercentage}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
         end
     end
     
-    render json: response.build_response
+    render json: response.build_response(session_end)
   end
 end
