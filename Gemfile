66999ac128f97280fb936529af88172d5019929f
watanabefg <h.w.watanabe@gmail.com>
2018-05-06 01:36:20 +0000

    launch_request

--- Gemfile
+++ Gemfile
@@ -55,6 +55,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari'
 
-#group :production do
-#  gem 'pg', '0.21.0'
-#end
+group :production do
+  gem 'pg', '0.21.0'
+end
--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,5 @@
 class TalksController < ApplicationController
-  protect_from_forgery with: :null_session
+  protect_from_forgery :except => [:create]
 
   # POST /talks
   def create
@@ -8,17 +8,25 @@ class TalksController < ApplicationController
     response = AlexaRubykit::Response.new
     session_end = true
     
-    case request.name
-      when 'WeightManagement'
-        weight = request.slots[:weight][:value]
-        response.add_speech("#{weight}キログラムですね。体脂肪率は何％ですか？")
-        response.add_session_attribute :weight, weight
+    response.add_speech("#{request.intent.name}")
+    
+    case request.type
+      when 'LAUNCH_REQUEST'
+        response.add_speech("スマートバルクへようこそ。体重と体脂肪率の管理をします。管理をしますか？")
         session_end = false
-      when 'BodyfatpercentageManagement'
-        weight = request.session.attributes[:weight]
-        bodyfatpercentage = request.slots[:bodyfatpercentage][:value]
-        # TODO: DB登録処理を入れる
-        response.add_speech("#{bodyfatpercentage}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
+      when 'INTENT_REQUEST'
+        case request.intent.name 
+          when 'WeightManagement'
+            weight = request.intent.slots[:weight][:value]
+            response.add_speech("#{weight}キログラムですね。体脂肪率は何％ですか？")
+            response.add_session_attribute :weight, weight
+            session_end = false
+          when 'BodyfatpercentageManagement'
+            weight = request.session.attributes[:weight]
+            bodyfatpercentage = request.intent.slots[:bodyfatpercentage][:value]
+            # TODO: DB登録処理を入れる
+            response.add_speech("#{bodyfatpercentage}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
+        end
     end
     
     render json: response.build_response
