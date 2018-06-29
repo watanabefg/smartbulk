6e3f043091650ad54726f6d1cd290cc00f4aa1ed
watanabefg <h.w.watanabe@gmail.com>
2018-06-29 15:31:31 +0000

    小数点以下対応

--- app/controllers/api/v1/talks_controller.rb
+++ app/controllers/api/v1/talks_controller.rb
@@ -35,27 +35,109 @@ class Api::V1::TalksController < Api::V1::ApiController#Grape::API
           when 'AMAZON.CancelIntent'
             response.add_speech("ご利用ありがとうございました。またスマートバルクをご利用ください。")
           when 'WeightManagement'
-            weight = request.slots[:weight][:value]
-            response.add_speech("#{weight}キログラムですね。体脂肪率は何％ですか？")
-            response.add_session_attribute :weight, weight
+            weight = getWeight(request)
+            
+            if weight == 0 then
+              response.add_speech("体重は何キログラムですか？")
+            else
+              if hasDot(request) then
+                dotnumber = getDotNumber(request)
+                
+                if dotnumber == 0 then
+                  response.add_speech("体重は何キログラムですか？")
+                else
+                  response.add_speech("#{weight}てん#{dotnumber}キログラムですね。体脂肪率は何％ですか？")
+                  weight = (weight.to_s + "." + dotnumber.to_s).to_f
+                  response.add_session_attribute :weight, weight
+                end
+              else
+                response.add_speech("#{weight}キログラムですね。体脂肪率は何％ですか？")
+                response.add_session_attribute :weight, weight
+              end
+            end
+            
             session_end = false
           when 'BodyfatpercentageManagement'
             weight = request.session.attributes[:weight]
-            bodyfatpercentage = request.slots[:bodyfatpercentage][:value]
+            bodyfatpercentage = getBodyFatPercentage(request)
             
-            con = ActiveRecord::Base.connection
-            sql = ActiveRecord::Base.send(
-              :sanitize_sql_array,
-              ['SELECT resource_owner_id from oauth_access_tokens WHERE token=?', request.session.user[:accessToken]]
-            )
-            resource_owner_id = con.select_value(sql)
-            record = Record.new(:user_id => resource_owner_id, :weight => weight, :fatPer => bodyfatpercentage)
-            if record.save
-              response.add_speech("#{bodyfatpercentage}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
+            if bodyfatpercentage == 0 then
+              response.add_speech("体脂肪率は何パーセントですか？")
+              session_end = false
+            else
+              if hasDot(request) then
+                dotnumber = getDotNumber(request)
+              
+                if dotnumber == 0 then
+                  response.add_speech("体脂肪率は何パーセントですか？")
+                  session_end = false
+                else
+                  fatPer = (bodyfatpercentage.to_s + "." + dotnumber.to_s).to_f
+                  resource_owner_id = getResourceOwnerId(request)
+                  record = Record.new(:user_id => resource_owner_id, :weight => weight, :fatPer => fatPer)
+                  
+                  if record.save
+                    response.add_speech("#{bodyfatpercentage}てん#{dotnumber}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
+                  end
+                end
+              else
+                fatPer = bodyfatpercentage
+                resource_owner_id = getResourceOwnerId(request)
+                record = Record.new(:user_id => resource_owner_id, :weight => weight, :fatPer => fatPer)
+                
+                if record.save
+                  response.add_speech("#{bodyfatpercentage}パーセントですね。登録しました。マイページにアクセスすると履歴が確認できます。")
+                end
+              end
             end
         end
     end
     
     render json: response.build_response(session_end)
+  end
+  
+  private
+  def getWeight(request)
+    if request.slots[:weight][:value].present? then
+      weight = request.slots[:weight][:value]
+      if /^[0-9]+$/ =~ weight
+        return weight
+      end
+    end
+    return 0
+  end
+  
+  def hasDot(request)
+    if request.slots[:dotnumber][:value].present? then
+      return true
+    end
+    return false
+  end
+      
+  def getDotNumber(request)
+    if request.slots[:dotnumber][:value].present? then
+      dotnumber = request.slots[:dotnumber][:value]
+      if /^[0-9]+$/ =~ dotnumber
+        return dotnumber
+      end
+    end
+  end
+  
+  def getBodyFatPercentage(request)
+    if request.slots[:bodyfatpercentage][:value].present? then
+      bodyfatpercentage = request.slots[:bodyfatpercentage][:value]
+      if /^[0-9]+$/ =~ bodyfatpercentage
+        return bodyfatpercentage
+      end
+    end
+  end
+ 
+  def getResourceOwnerId(request)
+    con = ActiveRecord::Base.connection
+    sql = ActiveRecord::Base.send(
+      :sanitize_sql_array,
+      ['SELECT resource_owner_id from oauth_access_tokens WHERE token=?', request.session.user[:accessToken]]
+    )
+    return con.select_value(sql)
   end
 end
