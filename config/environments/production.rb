a4d99f664b7a628c77f1516b05be2315df14359b
watanabefg <h.w.watanabe@gmail.com>
2018-11-09 06:59:21 +0000

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Use SendGrid - Add-ons - Heroku
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'gmail.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: 'gmail.com',
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 end
